4a601525a5757640c7b56dc968f095cba5cbeb65
Juan <jotakuun@gmail.com>
2017-10-19 22:05:56 +0200

    fetch data helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,17 @@
 import * as d3 from 'd3';
 
 export const apiHost = 'https:/api.github.com/';
+
+export function fetchData(url, params) {
+    return new Promise((resolve) => {
+        fetch(url)
+        .then((res) => res.json())
+        .then(function(res) {
+            console.log('res', res)
+            resolve(res)
+        })
+        .catch(function(err) {
+            console.log('err', err)
+        });
+    })
+}
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,10 +1,12 @@
-import { take, call, put, fork, race } from 'redux-saga/effects';
+import { take, call, put, fork, race, select } from 'redux-saga/effects';
 
 import {
     GET_REPOS_INFO, GET_REPOS_INFO_SUCCESS, GET_REPOS_INFO_FAILURE,
     GET_RADAR_DATA, GET_RADAR_DATA_SUCCESS, GET_RADAR_DATA_FAILURE,
     GET_POPULARITY_DATA, GET_POPULARITY_DATA_SUCCESS, GET_POPULARITY_DATA_FAILURE
 } from './actions';
+
+import { apiHost, fetchData } from '../helpers';
 
 function* getRadarData({ payload: { repos } }) {
     try {
@@ -13,19 +15,6 @@ function* getRadarData({ payload: { repos } }) {
     } catch (err) {
         yield put({ type: GET_RADAR_DATA_FAILURE, payload: err });
     }
-    // todo: simplify these d3.request with a helper
-    /* d3.request(apiHost + `repos/${repo.author}/${repo.name}`)
-    .mimeType("application/json")
-    .response((data) => JSON.parse(data.responseText))
-    .get((data) => {
-      this.props.getRadarData({ name: data.name,
-        forks: data.forks_count,
-        stars: data.stargazers_count,
-        subscribers: data.subscribers_count,
-        open_issues: data.open_issues_count,
-        url: data.html_url
-      })}
-    ); */
 }
 
 function* setData() {
@@ -34,9 +23,9 @@ function* setData() {
 }
 
 function* getReposInfo() {
-    // yield* take(GET_REPOS_INFO);
+    yield take(GET_REPOS_INFO)
     try {
-        const pickedRepos = yield take((state) => state.repos.pickedRepos);
+        const pickedRepos = yield select((state) => state.repos.pickedRepos);
         const data = yield call(apiRequests, pickedRepos);
         yield put({ type: GET_REPOS_INFO_SUCCESS, payload: data });
     } catch (err) {
@@ -44,23 +33,15 @@ function* getReposInfo() {
     }
 }
 
-// todo: move these api resolvers to another file
 function apiRequests(reposPicked) {
-    return new Promise((resolve) => {
-        d3.request(apiHost + `repos/${repo.author}/${repo.name}`)
-            .mimeType("application/json")
-            .response((data) => JSON.parse(data.responseText))
-            .get((data) => resolve(data));
-    });
+    let requestPromises = reposPicked.map((repo) =>{
+        return fetchData(apiHost + `repos/${repo.author}/${repo.name}`);
+    })
+    return Promise.all(requestPromises).then((data) => data)
 }
 
-
-
 export default function* rootSaga() {
-    // Wait for get all repos request
-    yield take(GET_REPOS_INFO, getReposInfo);
-
-    // todo
+    yield fork(getReposInfo);
 
     // yield fork(setData);
 }
