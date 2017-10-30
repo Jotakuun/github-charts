ae070dec96b06a613f961ce83297dbf128df2b5a
Juan <jotakuun@gmail.com>
2017-10-30 13:38:22 +0100

    radar canvas done

--- src/components/RadarChart/RadarCanvas/RadarCanvas.js
+++ src/components/RadarChart/RadarCanvas/RadarCanvas.js
@@ -13,6 +13,14 @@ export class RadarCanvas extends React.Component {
         console.log(nextProps)
     }
     render() {
+        let segments = this.props.segments.map((line, index) => {
+            return(
+            <line key={'segmentLine'+index} className={styles.RadarCanvas__Levels} 
+            x1={line.x1} y1={line.y1}
+            x2={line.x2} y2={line.y2}
+            transform={'translate(' + line.translateX +',' + line.translateY + ')' }/>
+            )
+        })
         let lines = this.props.lines.map((line, index) => {
             return(
                 <line key={'axisline'+index} className={styles.RadarCanvas__Axes} 
@@ -20,10 +28,18 @@ export class RadarCanvas extends React.Component {
                 x2={line.x2} y2={line.y2}/>
             )
         });
+        let levelsText = this.props.levelsText.map((value, index) => {
+            return(
+            <text key={'levelsText'+index} className={styles.RadarCanvas__Legend} 
+            x={value.x} y={value.y}
+            transform={'translate(' + value.translateX +',' + value.translateY + ')' }>{value.value}</text>
+            )
+        })
         return (
             <svg width={this.props.width} height={this.props.height}>
                 <g transform="translate(0,0)">
-                   
+                    {segments}
+                    {levelsText}
                     {lines}
                 </g>
             </svg>
--- src/components/RadarChart/RadarChart.js
+++ src/components/RadarChart/RadarChart.js
@@ -18,25 +18,69 @@ class RadarChart extends React.Component {
       levels: 4
     };
     this.canvasRadius = Math.min(this.canvas.width / 2, this.canvas.height / 2);
+    this.canvasSegments = [];
+    this.canvasLevelsText = [];
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.axis !== this.props.axis) {
+      this.createCanvasSegments(nextProps.axis);
+    }
+  }
+
+  createReactCanvas() {
+
+  }
+
+  createCanvasSegments(axis) {
+    this.canvasSegments = [];
+    this.canvasLevelsText = [];
+    const canvasRadius = Math.min(this.canvas.width / 2, this.canvas.height / 2);
+    //canvas segments
+    for (let i = 0; i < this.canvas.levels; i++) {
+      axis.forEach((data, index) => {
+        let levelFactor = canvasRadius * ((i + 1) / this.canvas.levels);
+        this.canvasSegments.push({
+          x1: levelFactor * (1 - Math.sin(index * 2 * Math.PI / axis.length)),
+          y1: levelFactor * (1 - Math.cos(index * 2 * Math.PI / axis.length)),
+          x2: levelFactor * (1 - Math.sin((index + 1) * 2 * Math.PI / axis.length)),
+          y2: levelFactor * (1 - Math.cos((index + 1) * 2 * Math.PI / axis.length)),
+          translateX: ((this.canvas.width / 2) - levelFactor),
+          translateY: ((this.canvas.height / 2) - levelFactor)
+        })
+      })
+    }
+
+    // levels text
+    for (let i = 0; i < this.canvas.levels; i++) {
+      let levelFactor = canvasRadius * ((i + 1) / this.canvas.levels);
+      this.canvasLevelsText.push({
+        value: ((i + 1) * 100 / this.canvas.levels).toFixed(0),
+        x: levelFactor * (1 - 1 * Math.sin(0)),
+        y: levelFactor * (1 - 1 * Math.cos(0)),
+        translateX: ((this.canvas.width / 2) - (levelFactor - 5)),
+        translateY: ((this.canvas.height / 2) - levelFactor)
+      })
+    }
+    
+  }
   render() {
     const radians = 2 * Math.PI;
     let lines = [];
     if (this.props.axis) {
       lines = this.props.axis.map((axis, i) => ({
-          x1: this.canvas.width / 2,
-          y1: this.canvas.height / 2,
-          x2: this.canvas.width / 2 * (1 - 1 * Math.sin(i * radians / this.props.axis.length)),
-          y2: this.canvas.height / 2 * (1 - 1 * Math.cos(i * radians / this.props.axis.length))
+        x1: this.canvas.width / 2,
+        y1: this.canvas.height / 2,
+        x2: this.canvas.width / 2 * (1 - 1 * Math.sin(i * radians / this.props.axis.length)),
+        y2: this.canvas.height / 2 * (1 - 1 * Math.cos(i * radians / this.props.axis.length))
       }));
-  }
+    }
     return (
       <Card>
         <div className={styles.RadarChart}>
           <h1>RadarChart_component</h1>
           <div className={styles.RadarChart__Canvas}>
-            <RadarCanvas width={this.canvas.width} height={this.canvas.height} lines={lines} />
+            <RadarCanvas width={this.canvas.width} height={this.canvas.height} lines={lines} segments={this.canvasSegments} levelsText={this.canvasLevelsText}/>
           </div>
           <button onClick={this.props.changeAxis}>change axis</button>
         </div>
